2e5c496175a69f5f821dfbd659365e5ad53f7e8d
DevendraBhatta <devendrabhatta63@gmail.com>
2025-12-27 09:10:08 +0545

    first-commit
    committing for first-time

---
+++ BizCard/app/src/main/java/com/devdeveloper/bizcard/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.devdeveloper.bizcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.devdeveloper.bizcard.ui.theme.BizCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BizCardTheme {
+                Surface {
+                    CreateBizCard()
+                }
+             }
+        }
+    }
+}
+
+@Composable fun CreateBizCard(){
+  Surface (modifier = Modifier.fillMaxWidth()
+      .fillMaxHeight()){
+      Card(modifier = Modifier.fillMaxWidth()
+          .height(390.dp)
+          .padding(12.dp),
+          colors = CardDefaults.cardColors(
+              containerColor = Color.White
+          ),
+          shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+          elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)) {
+      }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BizCardTheme {
+        CreateBizCard()
+    }
+}
