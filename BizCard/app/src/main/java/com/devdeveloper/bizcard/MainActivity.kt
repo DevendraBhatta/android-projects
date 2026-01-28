9a54c13451c6ad472d29c211cf5817de5b44a351
DevendraBhatta <devendrabhatta63@gmail.com>
2026-01-28 07:53:07 +0545

    Update MainActivity.kt

--- BizCard/app/src/main/java/com/devdeveloper/bizcard/MainActivity.kt
+++ BizCard/app/src/main/java/com/devdeveloper/bizcard/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,6 +28,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,6 +51,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -73,15 +81,19 @@ fun BizCard() {
                 CreateInfo()
                 Button(
                     onClick = {
-                        Log.d("Clicked", "BizCard: Clicked")
-
+                        buttonClickedState.value = !buttonClickedState.value
                     }
                 ) {
                     Text(
                         text = "Portfolio",
-                        style = MaterialTheme.typography.bodyMedium,
-
-                        )
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+                if (buttonClickedState.value) {
+                    Content()
+                } else {
+                    Box() {
+                    }
                 }
             }
         }
@@ -91,28 +103,34 @@ fun BizCard() {
 @Preview
 @Composable
 fun Content() {
-    Box(modifier = Modifier.fillMaxWidth()
-        .fillMaxHeight()
-        .padding(5.dp)){
-        Surface(modifier = Modifier.padding(3.dp)
+    Box(
+        modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(),
+            .fillMaxHeight()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
             shape = RoundedCornerShape(corner = CornerSize(6.dp)),
             border = BorderStroke(width = 2.dp, color = Color.LightGray)
         ) {
-            Portfolio(data = listOf("Project 1","Project 2","Project 3"))
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3", "Project 4"))
         }
     }
 
 }
 
 @Composable
-fun Portfolio(data:List<String>) {
+fun Portfolio(data: List<String>) {
 
-    Text(
-        text = "Portfolio"
-    )
-
+    LazyColumn {
+        items(data) { item ->
+            Text(item)
+        }
+    }
 }
 
 
